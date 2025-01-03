45199ff6ed83d0eb51b1d3f35a11975ec4df891d
OPortista <portista@protonmail.com>
2025-01-03 18:43:22 +0100

    add name fix

--- get-lyrics.sh
+++ get-lyrics.sh
@@ -100,6 +100,7 @@ format_name() {
     name=$(echo "$track" | jq -r '.name')
     name="${name//[\\\/?*:<>\"]/-}"
     name="${name/À/a}"
+    name="${name/’/'}"
 
     if [ -n "$secondary_artists" ]; then
         feat_artists=$(echo "$secondary_artists" | paste -sd ", " | sed 's/,/, /g')
